469d0a3b51c824c6bcf3392de30b5092cf2e9294
IshaBhatia05 <datasciencelearning05@gmail.com>
2025-11-02 12:12:29 +0530

    Leetcode : binary search code

--- searching/src/Main.java
+++ searching/src/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        int[] arr={3,5,4,-2,1,48,98};
-        int target=-2;
-        int value=linerSearch(arr,target);
-        System.out.print("Value present at what index: "+value);
+        int[] arr = {3, 5, 4, -2, 1, 48, 98};//array can be unsorted
+        int target = -2;
+        int value = linerSearch(arr, target);
+        System.out.println("Value present at what index in linear search: " + value);
+        //Binary Search Array has to be with sorted array
+        int[] arr1 = {-1, -2,0,1,2, 3, 4, 5, 6};
+        int value1 = binarySearch(arr1, target);
+        System.out.println("Value present at what index in Binary search: " + value1);
+
+
     }
     static int linerSearch(int[] arr,int target){
         if(arr.length==0){
