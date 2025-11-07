d14e7db708e7e067709f61bfd7ede731ad61e755
IshaBhatia05 <datasciencelearning05@gmail.com>
2025-11-07 13:45:55 +0530

    Leetcode : binary search questions

---
+++ leetcode_Binary_Search/src/leetcode_mountain_array_find_peak.java
@@ -0,0 +1,20 @@
+class leetcode_mountain_array_find_peak {
+    public static void main(String[] args){
+       int[] nums = {1,2,3,1};
+       System.out.println(findPeakElement(nums));
+    }
+    public static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid+1]) {
+                end = mid;
+
+            } else
+                start = mid + 1;
+        }
+        return end;
+    }
+
+    }
--- searching/src/binarySearch.java
+++ searching/src/binarySearch.java
@@ -6,10 +6,9 @@ public class binarySearch {
     }
     public static int search(int[] nums, int target) {
             int start =0;int end=nums.length-1;
-        int mid;
 
             while(start<=end){
-                 mid=start+(end-start)/2;
+                int mid=start+(end-start)/2;
                 if(target<nums[mid]){
                     end=mid-1;
                 }else if(target>nums[mid]){
