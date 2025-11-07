10831152f6be0c3551421f2999753ef8ad15b65f
IshaBhatia05 <datasciencelearning05@gmail.com>
2025-11-07 16:17:48 +0530

    cyclic sorting concepts

---
+++ sorting/src/cyclic_sort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class cyclic_sort {
+    public static void main(String[] args) {
+        int[] arr={5,4,3,1,2};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void cyclicSort(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct=arr[i]-1;//index of element in cyclic sort = value-1
+            if(arr[i]!=arr[correct]){
+                selection_sort.swap(arr,i,correct);
+            }
+                i++;
+
+        }
+
+
+    }
+}
